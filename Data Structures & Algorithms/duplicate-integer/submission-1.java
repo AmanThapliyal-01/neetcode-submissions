2fcfb33c1d37335fa0bf01b7e83505a31e56651d
Amun <49000269+AmanThapliyal-01@users.noreply.github.com>
2026-06-24 01:56:46 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> dict = new HashMap<>();
+        for(int i=0; i<nums.length ;i++)
+        {
+            if(dict.containsKey(nums[i]))
+            {
+                if(dict.get(nums[i])>=1)
+                    return true;
+                else
+                    dict.put(nums[i],dict.get(nums[i])+1);
+            }
+            else
+                dict.put(nums[i],1);
+        }
+        return false;
+    }
+}
