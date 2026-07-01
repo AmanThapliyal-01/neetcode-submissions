897d7315806cf9943c3c578deb91bd5cbe35e7eb
Amun <49000269+AmanThapliyal-01@users.noreply.github.com>
2026-07-02 00:47:10 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i =0;
+        int j=nums.length-1;
+        while(i!=j)
+        {
+            if(nums[i]>nums[j])
+            {
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+            else
+                j--;
+        }
+    }
+}
